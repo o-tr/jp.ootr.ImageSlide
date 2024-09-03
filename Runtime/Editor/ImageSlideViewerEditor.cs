09bac0640a5f9a3918751f07a02f8e71ab4baff8
ootr <git@ootr.jp>
2024-09-03 23:33:09 +0900

    エディタースクリプトをリファクタリング

--- Runtime/Editor/ImageSlideViewerEditor.cs
+++ Runtime/Editor/ImageSlideViewerEditor.cs
@@ -71,21 +71,7 @@ namespace jp.ootr.ImageSlide.Editor.Viewer
 
             if (!EditorGUI.EndChangeCheck()) return;
             script.SetSeekDisabled(script.seekDisabled);
-            var currentSyncObj = script.rootGameObject.GetComponent<VRCObjectSync>();
-            if (script.isObjectSyncEnabled)
-            {
-                if (currentSyncObj == null)
-                {
-                    script.rootGameObject.AddComponent<VRCObjectSync>();
-                }
-            }
-            else
-            {
-                if (currentSyncObj != null)
-                {
-                    DestroyImmediate(currentSyncObj);
-                }
-            }
+            ImageSlideViewerUtils.UpdateObjectSync(script);
 
             EditorUtility.SetDirty(script);
         }
@@ -110,7 +96,7 @@ namespace jp.ootr.ImageSlide.Editor.Viewer
             {
                 var imageSlideViewer = ComponentUtils.GetAllComponents<ImageSlideViewer>();
 
-                ImageSlideViewerUtils.ValidateImageSlide(imageSlideViewer.ToArray());
+                ImageSlideViewerUtils.ValidateImageSlides(imageSlideViewer.ToArray());
             }
         }
     }
@@ -123,30 +109,59 @@ namespace jp.ootr.ImageSlide.Editor.Viewer
         {
             var imageSlideViewer = ComponentUtils.GetAllComponents<ImageSlideViewer>();
 
-            return ImageSlideViewerUtils.ValidateImageSlide(imageSlideViewer.ToArray());
+            return ImageSlideViewerUtils.ValidateImageSlides(imageSlideViewer.ToArray());
         }
     }
 
     public static class ImageSlideViewerUtils
     {
-        public static bool ValidateImageSlide(ImageSlideViewer[] imageSlideViewers)
+        public static bool ValidateImageSlides(ImageSlideViewer[] imageSlideViewers)
         {
             var flag = true;
             foreach (var viewer in imageSlideViewers)
             {
-                if (viewer.imageSlide == null)
+                if (!ValidateImageSlide(viewer))
                 {
-                    Debug.LogWarning("ImageSlideViewer: ImageSlide is not assigned");
                     flag = false;
-                    continue;
                 }
-
-                if (viewer.imageSlide.listeners.Has(viewer)) continue;
-                viewer.imageSlide.listeners = viewer.imageSlide.listeners.Append(viewer);
-                EditorUtility.SetDirty(viewer.imageSlide);
             }
 
             return flag;
+        }
+
+        public static bool ValidateImageSlide(ImageSlideViewer imageSlideViewer)
+        {
+            if (imageSlideViewer.imageSlide == null)
+            {
+                Debug.LogWarning("ImageSlideViewer: ImageSlide is not assigned");
+                return false;
+            }
+
+            UpdateObjectSync(imageSlideViewer);
+
+            if (imageSlideViewer.imageSlide.listeners.Has(imageSlideViewer)) return true;
+            imageSlideViewer.imageSlide.listeners = imageSlideViewer.imageSlide.listeners.Append(imageSlideViewer);
+            EditorUtility.SetDirty(imageSlideViewer.imageSlide);
+            return true;
+        }
+
+        public static void UpdateObjectSync(ImageSlideViewer imageSlideViewer)
+        {
+            var currentSyncObj = imageSlideViewer.rootGameObject.GetComponent<VRCObjectSync>();
+            if (imageSlideViewer.isObjectSyncEnabled)
+            {
+                if (currentSyncObj == null)
+                {
+                    imageSlideViewer.rootGameObject.AddComponent<VRCObjectSync>();
+                }
+            }
+            else
+            {
+                if (currentSyncObj != null)
+                {
+                    Object.DestroyImmediate(currentSyncObj);
+                }
+            }
         }
     }
 }
