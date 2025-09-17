c8afaa175817bda2ff0fcb977fdf492b5cebf944
ootr <git@ootr.jp>
2025-09-18 03:08:23 +0900

    ロジックを単純化

--- Runtime/jp.ootr.ImageSlide/Scripts/Viewer/13_UISlide.cs
+++ Runtime/jp.ootr.ImageSlide/Scripts/Viewer/13_UISlide.cs
@@ -95,16 +95,15 @@ namespace jp.ootr.ImageSlide.Viewer
                 ConsoleDebug($"main texture not match: {_mainLoadedFileName} / {_mainLoadedSource} != {fileUrl} / {sourceUrl}");
                 return;
             }
-            
+
+            animator.SetBool(AnimatorIsLoading, false);
             var texture = controller.CcGetTexture(sourceUrl, fileUrl);
             if (texture == null) 
             {
                 ConsoleError($"Failed to get texture for {sourceUrl}/{fileUrl}");
-                animator.SetBool(AnimatorIsLoading, false);
                 return;
             }
-            
-            animator.SetBool(AnimatorIsLoading, false);
+
             slideMainView.texture = texture;
             slideMainViewFitter.aspectRatio = (float)texture.width / texture.height;
         }
