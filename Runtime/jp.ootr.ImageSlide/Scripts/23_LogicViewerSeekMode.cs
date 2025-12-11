489dfa0209fad36fe4c3d6f96015125ef3e958f4
ootr <git@ootr.jp>
2025-12-12 01:22:00 +0900

    コンパイルエラーを修正

--- Runtime/jp.ootr.ImageSlide/Scripts/23_LogicViewerSeekMode.cs
+++ Runtime/jp.ootr.ImageSlide/Scripts/23_LogicViewerSeekMode.cs
@@ -1,15 +1,12 @@
 ﻿using jp.ootr.ImageSlide.Viewer;
 using UnityEngine;
 using UnityEngine.UI;
-using VRC.SDK3.Data;
 using VRC.SDKBase;
-using VRC.Udon.Common.Interfaces;
 
 namespace jp.ootr.ImageSlide
 {
     public class LogicViewerSeekMode : LogicQueue
     {
-        [SerializeField] internal SeekMode seekMode;
         [SerializeField] private ToggleGroup seekModeToggleGroup;
         [SerializeField] private Toggle allowAllToggle;
         [SerializeField] private Toggle allowPreviousOnlyToggle;
@@ -66,34 +63,6 @@ namespace jp.ootr.ImageSlide
             base.SeekModeChanged(mode);
             seekMode = mode;
             UpdateToggleGroup();
-        }
-
-        protected override void DoSyncAll()
-        {
-            var dic = new DataDictionary();
-            dic.SetValue("type", (int)QueueType.SyncAll);
-            var sourceDic = new DataList();
-            var optionDic = new DataList();
-            for (var i = 0; i < Sources.Length; i++)
-            {
-                sourceDic.Add(Sources[i]);
-                optionDic.Add(Options[i]);
-            }
-
-            dic.SetValue("sources", sourceDic);
-            dic.SetValue("options", optionDic);
-            dic.SetValue("index", currentIndex);
-            dic.SetValue("seekMode", new DataToken((int)seekMode));
-
-            if (!VRCJson.TrySerializeToJson(dic, JsonExportType.Minify, out var json))
-            {
-                ConsoleError($"failed to serialize sync all json: {json}");
-                ProcessQueue();
-                return;
-            }
-
-            AddSyncQueue(json.String);
-            ProcessQueue();
         }
     }
 }
